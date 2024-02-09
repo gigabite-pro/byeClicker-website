ba2747fd95635224334d48952bec781ef7157a60
Vaibhav Sharma <rajmachawal19@gmail.com>
2024-02-08 16:00:19 -0800

    made public

--- app.js
+++ app.js
@@ -3,29 +3,29 @@
 const HOST = 'https://bye-clicker-api.vercel.app';
 const FRONTEND_HOST = 'https://byeclicker.com';
 
-function changeToSubscribe() {
-    const token = localStorage.getItem('byeClicker_jwt');
-    if (token === null) {
-        window.location.href = `${HOST}/auth/login`;
-    } else {
-        fetch(`${HOST}/auth/verify`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ token: token})
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.location.href = `${FRONTEND_HOST}/subscribe?token=${data.token}`;
-            } else {
-                localStorage.removeItem('byeClicker_jwt');
-                window.location.href = `${HOST}/auth/login`;
-            }
-        }).catch((error) => { console.error('Error:', error); });
-    }
-}
+// function changeToSubscribe() {
+//     const token = localStorage.getItem('byeClicker_jwt');
+//     if (token === null) {
+//         window.location.href = `${HOST}/auth/login`;
+//     } else {
+//         fetch(`${HOST}/auth/verify`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ token: token})
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.success) {
+//                 window.location.href = `${FRONTEND_HOST}/subscribe?token=${data.token}`;
+//             } else {
+//                 localStorage.removeItem('byeClicker_jwt');
+//                 window.location.href = `${HOST}/auth/login`;
+//             }
+//         }).catch((error) => { console.error('Error:', error); });
+//     }
+// }
 
 function showExtension() {
     window.location.href = 'https://chromewebstore.google.com/detail/byeclicker/kmnlnlkcacgjkngggifcnalkldgpflic';
